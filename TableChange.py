d47d03ffb711e1bb53da7255f03631798feb4bac
Arne De Veylder <arne.de.veylder@telenet.be>
2023-01-12 20:30:43 +0100

    Added ToDo list

---
+++ TableChange.py
@@ -0,0 +1,13 @@
+""" table_change
+Script to get (scrape) the current standings of the Belgium football
+competition. Also get the fixtures of the next matchday and loop over every
+possible score combination. The program returns a graph (lollipop) chart with
+the maximum change in position
+for each team
+
+TODO: 1. Scrape site to get the current table
+TODO: 2. Scrape every fixture of next matchday
+TODO: 3. Go over all possible score combinations and check
+TODO: changes in the standings.
+TODO: 4. Create lollipop chart
+"""
